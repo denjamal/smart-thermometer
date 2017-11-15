dd90a2be41bc79762c748ca26191bde1418d5bb5
Denis Malimon <denis.malimon@clarity-ag.net>
2017-11-15 17:11:52 +0200

    Add basic HTML page Add Json library

--- smartTermometer.ino
+++ smartTermometer.ino
@@ -6,6 +6,7 @@
 #include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
 #include <ESP8266HTTPClient.h>
+#include <ArduinoJson.h>
 
 #define DHTPIN 2        
 #define DHTTYPE DHT11
@@ -16,6 +17,73 @@ IPAddress ip(172,27,28,149);
 IPAddress gateway(172, 27, 29, 254);
 IPAddress subnet(255, 255, 255, 0);
 
+const char * indexHTML  = "<!DOCTYPE html>"
+"<html lang=\"en\">"
+
+"<head>"
+    "<title>ClarityAg</title>"
+    "<link href=\"https://fonts.googleapis.com/css?family=Space+Mono:400,700|Teko\" rel=\"stylesheet\">"
+    "<script src=\"https://code.jquery.com/jquery-3.2.1.min.js\" integrity=\"sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=\""
+        "crossorigin=\"anonymous\"></script>"
+    "<style>"
+        "html {"
+            "height: 100%;"
+        "}"
+
+        "body {"
+            "height: 100%;"
+            "background: url(http://www.zastavki.com/pictures/1920x1200/2010/Computers_Server_019601_.jpg) center center no-repeat;"
+            "background-size: cover;"
+            "padding: 0px;"
+        "}"
+
+        "h2,"
+        "h3 {"
+            "font-family: 'Space Mono', monospace;"
+            "color: white;"
+            "text-transform: uppercase;"
+            "text-align: center;"
+            "font-size: 70px;"
+        "}"
+
+        "h3 {"
+            "font-size: 60px;"
+        "}"
+
+        ".data {"
+            "position: absolute;"
+            "top: 50%;"
+            "left: 0;"
+            "right: 0;"
+            "margin-top: -200px;"
+        "}"
+    "</style>"
+"</head>"
+  "<body>"
+      "<div class=\"data\">"
+          "<h2>temperature: <span id=\"temperature\"></span> &#8451;</h2>"
+          "<h3>humidity: <span id=\"humidity\"></span> %</h3>"
+      "</div>"
+  "</body>"
+  "<script>"
+      "$(document).ready(function(){"
+          "$.ajax({"
+              "url: \"getTemp\","
+              "method: \"Get\","
+              "dataType: \"json\""
+          "})"
+          ".done(function(data){"
+              "debugger;"
+              "$(\"#temperature\").text(data.temperature);"
+              "$(\"#humidity\").text(data.humidity);"
+          "})"
+          ".fail(function(){"
+              "alert(\"Error: Please contact someone\");"
+          "});"
+      "});"
+  "</script>"
+"</html>";
+
 const char* ssid = "clarity-ag";
 const char* password = "zflvbyrj";
 int temp;
@@ -24,8 +92,12 @@ String WebString;
 bool isTempHigh  = false;
 
 void handleRoot() {
+  server.send(200, "text/html", indexHTML);
+}
+
+void getTemperature() {
   GetTemp();
-  server.send(200, "text/plain", WebString);
+  server.send(200, "application/json", WebString);
 }
 
 void handleNotFound(){
@@ -44,10 +116,14 @@ void handleNotFound(){
 }
 
 void GetTemp (){
+  StaticJsonBuffer<200> jsonBuffer;
+  WebString = "";
   temp  = dht.readTemperature(); 
   humidity = dht.readHumidity();
-  WebString = "Temperature: " + String((int)temp)+"C \n";
-  WebString += "Humidity: " + String((int)humidity)+"% \n";
+  JsonObject& root = jsonBuffer.createObject();
+  root["temperature"] = (int)temp;
+  root["humidity"] = (int)humidity;
+  root.printTo(WebString);
   Serial.println(WebString);
 
   if(temp > 20 && !isTempHigh) {
@@ -104,6 +180,7 @@ void setup(void){
   }
 
   server.on("/", handleRoot);
+  server.on("/getTemp", getTemperature);
   server.onNotFound(handleNotFound);
   server.begin();
   Serial.println("HTTP server started");
